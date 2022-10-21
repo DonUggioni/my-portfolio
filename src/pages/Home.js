9470f48d5a5a520d43e545c0bb874797388aa928
DonUggioni <renan.uggioni@gmail.com>
2022-10-21 09:58:38 +0100

    Initial files upload

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './Home.module.scss';
+
+function Home() {
+  return <div className={classes.section_wrapper}>Home</div>;
+}
+
+export default Home;
