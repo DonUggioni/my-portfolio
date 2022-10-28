c0b5ea33b37444488ccc9bbaf89e147c100828c8
DonUggioni <renan.uggioni@gmail.com>
2022-10-28 15:28:09 +0100

    Implement emailJS functionality

--- src/components/contact-form/ContactForm.js
+++ src/components/contact-form/ContactForm.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+
 import Button from '../button/Button';
 import Footer from '../footer/Footer';
 
 import classes from './ContactForm.module.scss';
 
 function ContactForm() {
+  const form = useRef();
+  const [messageSent, setMessageSent] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false);
+
+  function Message(props) {
+    return (
+      <div className={classes.message_container}>
+        <h2>{props.message}</h2>
+      </div>
+    );
+  }
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_ffphtqq',
+        'template_20ax4n90',
+        form.current,
+        'SKeN1W0pSgyJKlshZ'
+      )
+      .then(
+        (result) => {
+          result && setMessageSent(true);
+        },
+        (error) => {
+          error && setErrorMessage(true);
+        }
+      );
+  };
+
   return (
     <section className={classes.form_container}>
       <div className={classes['form_inner-wrapper']}>
         <h3 className={classes.heading}>Contact Me</h3>
-        <form className={classes.form}>
+        <form className={classes.form} ref={form} onSubmit={sendEmail}>
           <label>Name</label>
           <input type="text" name="user_name" required />
 
@@ -23,6 +57,8 @@ function ContactForm() {
             Submit
           </Button>
         </form>
+        {messageSent && <Message message={'Thank you!'} />}
+        {errorMessage && <Message message={'Something went wrong'} />}
       </div>
       <Footer />
     </section>
